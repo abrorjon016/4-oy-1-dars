04417f8da63b4e3b06f2739c6cab555c2193824e
abrorjon016 <ikromjonovabrorjon3@gmail.com>
2024-07-16 22:43:35 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let num = +prompt("uzunlikni kiriting");
+let result = num / 100;
+
+console.log(result);
+//
+
+let num = +prompt("og'rlikni  kiriting");
+let result = num / 1000;
+
+console.log(result);
+//
+let num = +prompt("fayl hajmini  kiriting");
+let result = num / 1024;
+
+console.log(result);
